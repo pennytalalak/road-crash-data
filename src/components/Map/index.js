b9f41017efcd1355f3210a89a1e7b767d3641868
pennytalalak <36777961+pennytalalak@users.noreply.github.com>
2019-02-04 21:45:39 +1100

    mapping geojson with deck.gl

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,47 +1,64 @@
-import React, { Component } from 'react'
-import MapGL, { NavigationControl } from 'react-map-gl'
+import React, { Component } from "react";
+import { StaticMap } from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
 
-const TOKEN =
-  'pk.eyJ1IjoicGVubnlwYW5nY29kZSIsImEiOiJjanJpb2dmbWUwM3p0M3ptYTk0a2N4MXBoIn0.HoDSIfRA2aPEH5zxXt8T2Q'
+//Mapbox Token
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoicGVubnlwYW5nY29kZSIsImEiOiJjanJpb2dmbWUwM3p0M3ptYTk0a2N4MXBoIn0.HoDSIfRA2aPEH5zxXt8T2Q";
 
-const navStyle = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  padding: '10px',
-}
+//Geojson Data URL
+const DATA =
+  "https://raw.githubusercontent.com/pennytalalak/road-crash-data/master/src/data/output.geojson";
+
+const LIGHT_SETTINGS = {
+  lightsPosition: [-125, 50.5, 5000, -122.8, 48.5, 8000],
+  ambientRatio: 0.2,
+  diffuseRatio: 0.5,
+  specularRatio: 0.3,
+  lightsStrength: [2.0, 0.0, 1.0, 0.0],
+  numberOfLights: 2
+};
+
+const INITIAL_VIEW_STATE = {
+  latitude: -35.280937,
+  longitude: 149.130005,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
 
 export default class Map extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      viewport: {
-        latitude: -25.2744,
-        longitude: 133.7751,
-        zoom: 2.8,
-        bearing: 0,
-        pitch: 0,
-      },
-    }
+  renderLayer() {
+    const { data = DATA } = this.props;
+
+    return [
+      new GeoJsonLayer({
+        id: "geoJson",
+        data,
+        filled: true,
+        lightSettings: LIGHT_SETTINGS
+      })
+    ];
   }
 
-  _onViewportChange = viewport => this.setState({ viewport })
-
   render() {
-    const { viewport } = this.state
+    const { viewState, baseMap = true } = this.props;
+
     return (
-      <MapGL
-        {...viewport}
-        height="100%"
-        width="100%"
-        mapStyle="mapbox://styles/mapbox/dark-v9"
-        mapboxApiAccessToken={TOKEN}
-        onViewportChange={this._onViewportChange}
+      <DeckGL
+        layers={this.renderLayer()}
+        initialViewState={INITIAL_VIEW_STATE}
+        viewState={viewState}
+        controller={true}
       >
-        <div className="nav" style={navStyle}>
-          <NavigationControl />
-        </div>
-      </MapGL>
-    )
+        {baseMap && (
+          <StaticMap
+            mapStyle="mapbox://styles/mapbox/dark-v9"
+            preventStyleDiffing={true}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+        )}
+      </DeckGL>
+    );
   }
 }
