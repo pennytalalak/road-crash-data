5fe20150d6ffac42ebe3812ccd2d0b1973ea67cc
Penny Talalak <Penny.Talalak@arup.com>
2019-02-05 16:51:04 +1100

    new way of graphing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,67 @@
 import React, { Component } from 'react'
-import './App.css'
-import 'mapbox-gl/dist/mapbox-gl.css'
+import DeckGL from 'deck.gl'
+import { StaticMap } from 'react-map-gl'
+import { renderLayers } from '../src/components/Map'
+import taxiData from './data/taxi'
+import roadData from './data/road-data'
 
-import Map from '../src/components/Map'
+const MAPBOX_TOKEN =
+  'pk.eyJ1IjoicGVubnlwYW5nY29kZSIsImEiOiJjanJpb2dmbWUwM3p0M3ptYTk0a2N4MXBoIn0.HoDSIfRA2aPEH5zxXt8T2Q'
 
-class App extends Component {
+const INITIAL_VIEW_STATE = {
+  longitude: -74,
+  latitude: 40.7,
+  zoom: 11,
+  minZoom: 5,
+  maxZoom: 16,
+  pitch: 0,
+  bearing: 0,
+}
+
+export default class App extends Component {
+  state = {
+    points: [],
+    style: 'mapbox://styles/mapbox/dark-v9',
+  }
+
+  componentDidMount() {
+    this._processData()
+  }
+
+  _processData() {
+    const points = taxiData.reduce((accu, curr) => {
+      accu.push({
+        position: [Number(curr.pickup_longitude), Number(curr.pickup_latitude)],
+        pickup: true,
+      })
+      accu.push({
+        position: [
+          Number(curr.dropoff_longitude),
+          Number(curr.dropoff_latitude),
+        ],
+        pickup: false,
+      })
+      return accu
+    }, [])
+    this.setState({
+      points,
+    })
+  }
+
   render() {
-    const containerStyle = {
-      height: '100%',
-    }
     return (
-      <div style={containerStyle}>
-        <Map />
+      <div>
+        <DeckGL
+          layers={renderLayers({ data: this.state.points })}
+          initialViewState={INITIAL_VIEW_STATE}
+          controller
+        >
+          <StaticMap
+            mapStyle={this.state.style}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+        </DeckGL>
       </div>
     )
   }
 }
-
-export default App
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,64 +1,26 @@
-import React, { Component } from "react";
-import { StaticMap } from "react-map-gl";
-import DeckGL, { GeoJsonLayer } from "deck.gl";
+import { ScatterplotLayer } from 'deck.gl'
 
-//Mapbox Token
-const MAPBOX_TOKEN =
-  "pk.eyJ1IjoicGVubnlwYW5nY29kZSIsImEiOiJjanJpb2dmbWUwM3p0M3ptYTk0a2N4MXBoIn0.HoDSIfRA2aPEH5zxXt8T2Q";
+const PICKUP_COLOR = [114, 19, 108]
+const DROPOFF_COLOR = [243, 185, 72]
 
-//Geojson Data URL
-const DATA =
-  "https://raw.githubusercontent.com/pennytalalak/road-crash-data/master/src/data/output.geojson";
+const DATA_URL =
+  'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'
 
-const LIGHT_SETTINGS = {
-  lightsPosition: [-125, 50.5, 5000, -122.8, 48.5, 8000],
-  ambientRatio: 0.2,
-  diffuseRatio: 0.5,
-  specularRatio: 0.3,
-  lightsStrength: [2.0, 0.0, 1.0, 0.0],
-  numberOfLights: 2
-};
-
-const INITIAL_VIEW_STATE = {
-  latitude: -35.280937,
-  longitude: 149.130005,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0
-};
-
-export default class Map extends Component {
-  renderLayer() {
-    const { data = DATA } = this.props;
-
-    return [
-      new GeoJsonLayer({
-        id: "geoJson",
-        data,
-        filled: true,
-        lightSettings: LIGHT_SETTINGS
-      })
-    ];
-  }
-
-  render() {
-    const { viewState, baseMap = true } = this.props;
-
-    return (
-      <DeckGL
-        layers={this.renderLayer()}
-        initialViewState={INITIAL_VIEW_STATE}
-        viewState={viewState}
-        controller={true}
-      >
-        {baseMap && (
-          <StaticMap
-            mapStyle="mapbox://styles/mapbox/dark-v9"
-            preventStyleDiffing={true}
-            mapboxApiAccessToken={MAPBOX_TOKEN}
-          />
-        )}
-      </DeckGL>
-    );
-  }
+export function renderLayers(props) {
+  const { data = DATA_URL, onHover, settings } = props
+  return [
+    new ScatterplotLayer({
+      id: 'scatterplot',
+      getPosition: d => d.position,
+      getColor: d => (d.pickup ? PICKUP_COLOR : DROPOFF_COLOR),
+      getRadius: d => 5,
+      opacity: 0.5,
+      pickable: true,
+      radiusMinPixels: 0.25,
+      radiusMaxPixels: 30,
+      data,
+      onHover,
+      ...settings,
+    }),
+  ]
 }
