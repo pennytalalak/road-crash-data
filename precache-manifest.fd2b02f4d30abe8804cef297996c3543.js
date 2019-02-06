5d787954ef2326969c2d73617cbc801e92c215cf
pennytalalak <36777961+pennytalalak@users.noreply.github.com>
2019-02-07 07:16:09 +1100

    Updates

---
+++ precache-manifest.fd2b02f4d30abe8804cef297996c3543.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d64086669d6f859022c3",
+    "url": "/road-crash-data/static/js/main.d6408666.chunk.js"
+  },
+  {
+    "revision": "6d2c3cb4ab69c24ee6ef",
+    "url": "/road-crash-data/static/js/1.6d2c3cb4.chunk.js"
+  },
+  {
+    "revision": "cf9c3051b174e6c71612",
+    "url": "/road-crash-data/static/js/runtime~main.cf9c3051.js"
+  },
+  {
+    "revision": "f2ea5bf0100dad978eb3021c64f321be",
+    "url": "/road-crash-data/static/media/ACT_Road_Crash_Data.f2ea5bf0.csv"
+  },
+  {
+    "revision": "7ed9decf73364d5316418b4c2f09a1df",
+    "url": "/road-crash-data/index.html"
+  }
+];
